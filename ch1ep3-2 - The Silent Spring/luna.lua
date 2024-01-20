58d00279e513194cfe1c0d4e7da0839700b03bb0
gspezi15 <75540750+gspezi15@users.noreply.github.com>
2024-01-20 13:47:42 +0000

    Silent River split pt 1

---
+++ ch1ep3-2 - The Silent Spring/luna.lua
@@ -0,0 +1,26 @@
+local areaNames = require("areanames")
+
+--Change this in your lua file to have automatically appearing messages on section switch for specific sections:
+areaNames.sectionNames = {
+	[0] = "The Silent Spring",
+        [1] = "Riverboat Interior",
+		[2] = "",
+        [3] = "Lumia City Outskirts",
+        [4] = "",
+        [5] = "",
+        [6] = "",
+        [7] = "",
+        [8] = "",
+        [9] = "",
+        [10] = "",
+        [11] = "",
+        [12] = "",
+        [13] = "",
+        [14] = "",
+        [15] = "",
+        [16] = "",
+        [17] = "",
+        [18] = "",
+        [19] = "",
+        [20] = ""
+        }
